68741435176423e7963ae3b7c2a3ba93cfda6021
Aircraft-King <1069865680@qq.com>
2020-04-03 21:36:43 +0800

    4-3

---
+++ 今天星期几.py
@@ -0,0 +1,17 @@
+a = input("请输入字母：")
+
+if a == 'm' :
+    print("星期一")
+elif a == 't' :
+    print("星期二")
+elif a == 'w' :
+    print("星期三")
+elif a == 't' :
+    print("星期四")
+elif a == 'f' :
+    print("星期五")
+elif a == 's' :
+    a = input("请输入第二个字母：")
+    if a == 'a' :    print("星期六")
+    else: print("星期天")
+input(' ')
